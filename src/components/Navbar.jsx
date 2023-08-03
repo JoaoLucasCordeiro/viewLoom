7b198d23d29f0ad41fb944de8b1f01eadb2feede
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-08-02 22:47:09 -0300

    Logo changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 
-import { logo } from '../utils/constants'
+import logo from '../utils/images/ViewLoomLogo.png'
 import SearchBar from './SearchBar'
 
 const Navbar = () => {
@@ -10,12 +10,14 @@ const Navbar = () => {
       alingItems="center"
       p={2} sx={{ position: 'stick', background: '#000', top: 0, justifyContent: 'space-between' }}>
 
+
       <Link to="/" style={{ display: 'flex', alingItems: 'center' }}>
-        <img src={logo} alt='Logo' height={45} />
+        <img src={logo} alt='Logo' height={130} />
       </Link>
 
-      <SearchBar/>
-
+      <div style={{marginTop: '40px'}}>
+        <SearchBar />
+      </div>
     </Stack>
   )
 }
