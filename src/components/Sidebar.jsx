2f317d6b17225deb50c86631664dd0c134f3cbad
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-08-03 23:24:33 -0300

    Configuration of the rapidAPI - finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import { Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
-const selectedCategory = 'Novos'
-
-const Sidebar = () => {
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
     return (
         <Stack direction='row' sx={{ overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' } }}>
             {categories.map((category) => (
                 <button className='category-btn'
+                onClick={() => {
+                    setSelectedCategory(category.name)
+                }}
                     style={{ background: category.name === selectedCategory && '#815ABF', color: 'white' }}
                     ket={category.name}
                 >
